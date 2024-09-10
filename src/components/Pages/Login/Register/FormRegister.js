391b8c3802851668325745fb0a7d97e6bc2e6d8b
Joaoprg09 <slatrabalho777@gmail.com>
2024-09-09 21:25:57 -0300

    update Login Register

--- src/components/Pages/Login/Register/FormRegister.js
+++ src/components/Pages/Login/Register/FormRegister.js
@@ -1,8 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useRef } from "react";
 import { clickEye } from "../../../utilits";
 
 export default function FormRegister() {
-  const [inputType, setInputType] = useState("password");
+  const [passType, setPassType] = useState("password");
+  const [confPassType, setConfPassType] = useState("password");
+  const [registerStage, setRegisterStage] = useState(1);
+  const confirmPassword = useRef(null);
 
   //State que contém as infos de registro
   const [formInfos, setFormInfos] = useState({
@@ -12,102 +15,130 @@ export default function FormRegister() {
   });
 
   return (
-    <form>
-      {
-        //campo de nome
-      }
-      <article className="FormDiv">
-        <label htmlFor="Nome">Nome:</label>
-        <div className="inputGroup">
-          <input
-            type="text"
-            name="Nome"
-            id="Nome"
-            required
-            autoFocus
-            onChange={(e) =>
-              setFormInfos((prevFormInfos) => {
-                return { ...prevFormInfos, Name: e.target.value };
-              })
+    <>
+      {registerStage === 1 ? (
+        <form>
+          {
+            //campo de nome
+          }
+          <article className="FormDiv">
+            <label htmlFor="Nome">Nome:</label>
+            <div className="inputGroup">
+              <input
+                type="text"
+                name="Nome"
+                id="Nome"
+                required
+                autoFocus
+                onChange={(e) =>
+                  setFormInfos((prevFormInfos) => {
+                    return { ...prevFormInfos, Name: e.target.value };
+                  })
+                }
+              />
+              <i className="bi bi-person-fill"></i>
+            </div>
+          </article>
+          {
+            //campo de email
+          }
+          <article className="FormDiv">
+            <label htmlFor="Email">Email:</label>
+            <div className="inputGroup">
+              <input
+                type="email"
+                name="Email"
+                id="Email"
+                required
+                onChange={(e) =>
+                  setFormInfos((prevFormInfos) => {
+                    return { ...prevFormInfos, Email: e.target.value };
+                  })
+                }
+              />
+              <i className="bi bi-envelope-at"></i>
+            </div>
+          </article>
+          {
+            //botão de registra a conta
+            //mudar depois para continuar
+          }
+          <article className="FormDiv contCenter">
+            <button
+              type="button"
+              id="submmitBtn"
+              onClick={(e) => setRegisterStage(2)}
+            >
+              Continuar
+            </button>
+          </article>
+        </form>
+      ) : (
+        <form>
+          {
+            //campo de senha provisorio,
+            //vou colocar em outra seção da pagina register com um input de comfirmar senha
+          }
+          <article className="FormDiv">
+            <label htmlFor="pass">Senha:</label>
+            <div className="inputGroup">
+              <input
+                type={passType}
+                name="pass"
+                id="pass"
+                required
+                min={8}
+                onChange={(e) =>
+                  setFormInfos((prevFormInfos) => {
+                    return { ...prevFormInfos, password: e.target.value };
+                  })
+                }
+              />
+              <i
+                className="bi bi-eye-slash-fill"
+                onClick={(e) =>
+                  clickEye(e, {
+                    state: { value: passType, setValue: setPassType },
+                  })
+                }
+              ></i>
+            </div>
+          </article>
+          {
+            //alerta de invalit password
+          }
+          <article
+            className={
+              formInfos.password.length < 8
+                ? "FormDiv warning"
+                : "FormDiv warning disable"
             }
-          />
-          <i className="bi bi-person-fill"></i>
-        </div>
-      </article>
-      {
-        //campo de email
-      }
-      <article className="FormDiv">
-        <label htmlFor="Email">Email:</label>
-        <div className="inputGroup">
-          <input
-            type="email"
-            name="Email"
-            id="Email"
-            required
-            autoFocus
-            onChange={(e) =>
-              setFormInfos((prevFormInfos) => {
-                return { ...prevFormInfos, Name: e.target.value };
-              })
-            }
-          />
-          <i className="bi bi-envelope-at"></i>
-        </div>
-      </article>
-      {
-        //campo de senha provisorio,
-        //vou colocar em outra seção da pagina register com um input de comfirmar senha
-      }
-      <article className="FormDiv">
-        <label htmlFor="pass">Senha:</label>
-        <div className="inputGroup">
-          <input
-            type={inputType}
-            name="pass"
-            id="pass"
-            required
-            min={8}
-            onChange={(e) =>
-              setFormInfos((prevFormInfos) => {
-                return { ...prevFormInfos, password: e.target.value };
-              })
-            }
-          />
-          <i
-            className="bi bi-eye-slash-fill"
-            onClick={(e) =>
-              clickEye(e, {
-                state: { value: inputType, setValue: setInputType },
-              })
-            }
-          ></i>
-        </div>
-      </article>
-      {
-        //alerta de invalit password
-      }
-      <article
-        className={
-          formInfos.password.length < 8
-            ? "FormDiv warning"
-            : "FormDiv warning  disable"
-        }
-      >
-        <p>Senha muito curta</p>
-      </article>
-      {
-        //botão de registra a conta
-        //mudar depois para continuar
-      }
-      <article className="FormDiv contCenter">
-        <button
-          type="submmit"
-          disabled={formInfos.password.length < 8 ? true : false}
-        >
-          Criar
-        </button>
-      </article>
-    </form>
+          >
+            <p>A senha precisa ter 8 ou mais caracters</p>
+          </article>
+          <article className="FormDiv">
+            <label htmlFor="confirmPass">Confirmar senha:</label>
+            <div className="inputGroup">
+              <input
+                ref={confirmPassword}
+                type={confPassType}
+                name="confirmPass"
+                id="confirmPass"
+                required
+                min={8}
+              />
+              <i
+                className="bi bi-eye-slash-fill"
+                onClick={(e) =>
+                  clickEye(e, {
+                    state: { value: confPassType, setValue: setConfPassType },
+                  })
+                }
+              ></i>
+            </div>
+          </article>
+        </form>
+      )}
+    </>
   );
 }

eca7da47cdac75e023dafabac18925c73a0eb0cb
Alexandre Mesquita <alexandre.mdo23@gmail.com>
2024-09-20 12:25:07 -0300

    Backup 20/09

---
+++ src/API/mediaStack.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const API_NEWS = () => {
+  const [noticias, setNoticias] = useState([]);
+  const [carregando, setCarregando] = useState(true);
+  const [erro, setErro] = useState(null);
+
+  const API_KEY = '822e1276902c165085dad659017b0a79'
+  const API_URL = `https://api.mediastack.com/v1/news?access_key=${API_KEY}&countries=br&sort=popularity`
+
+  useEffect(() => {
+    const fetchNoticias = async () => {
+      try {
+        const response = await axios.get(API_URL)
+        setNoticias(response.data.data);
+      } catch (err) {
+        setErro('Erro ao carregar notícias.' . err.message);
+      } finally {
+        setCarregando(false);
+      }
+    };
+
+    fetchNoticias();
+  }, []);
+
+  return {noticias, carregando, erro};
+  // return (
+  //   <div>
+  //     <h1>Últimas Notícias</h1>
+  //     <ul>
+  //       {noticias.map((noticia, index) => (
+  //         <li key={index}>
+  //           <h2>{noticia.title}</h2>
+  //           {noticia.image && (
+  //             <img src={noticia.image} alt={noticia.title} />
+  //           )}
+  //           <p>{noticia.description}</p>
+  //           <b>{noticia.category}</b>
+  //           <a href={noticia.url} target="_blank" rel="noopener noreferrer">Leia mais</a>
+  //           <hr />
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   </div>
+  // );
+};
+
+export default API_NEWS;
--- src/components/Pages/Home/components/suasNoticias/noticiaPrincipal.js
+++ src/components/Pages/Home/components/suasNoticias/noticiaPrincipal.js
@@ -1,15 +1,25 @@
 import image from '../../../../../img/foto-noticia.jpg';
+import API_NEWS from '../../../../../API/mediaStack';
 
 export default function NoticiaPrincipal({ size, data }) {
-  const noticias = Array.isArray(data) ? [...data] : [];
+  const {noticias, carregando, erro} = API_NEWS();
+
+  if (carregando) return <p>Carregando...</p>;
+  if (erro) return <p>Erro ao carregar notícias: {erro}</p>;
+
+  // Verifique se existem notícias suficientes
+  if (!noticias || noticias.length < 2) {
+    return <p>Não há notícias disponíveis.</p>;
+  }
+
   return (
     <div className="pNoticia">
-      <a href="">
-        <img src={image} alt={noticias[0].title} width={size} height={size} />
-        <span>{noticias[0].site}</span>
+      <a href={noticias[0].url}>
+        <img src={noticias[0].image} alt={noticias[0].title} width={size} height={size} />
+        <span>{noticias[0].source}</span>
         <h2>{noticias[0].title}</h2>
         <small>
-          {noticias[0].horarioPostagem} - {noticias[0].autor}
+          {new Date(noticias[0].published_at).toLocaleDateString()} - {noticias[0].author}
           {/* Depois colocar os horários da postegem da noticia */}
         </small>
       </a>

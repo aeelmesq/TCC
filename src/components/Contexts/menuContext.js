4754c16be605242ef32468db64479b68fafc5ee5
Alexandre Mesquita <111612651+aeelmesq@users.noreply.github.com>
2024-10-21 16:06:52 -0300

    add context

---
+++ src/components/Contexts/menuContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const menu = createContext(null);
+
+export default menu;
---
+++ src/components/Contexts/menuMoreContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const menuMoreContext = createContext(null);
+
+export default menuMoreContext;
